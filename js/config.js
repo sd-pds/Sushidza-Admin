5ce35b9ec8dcfcbab345d912fc52e30ed018db24
СЫТНАЯ ДОСТАВКА <sytdostavka2@gmail.com>
2026-04-11 01:17:41 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.ADMIN_CONFIG = {
-  workerUrl: 'https://REPLACE-WITH-SUSHIDZA-ADMIN-WORKER.workers.dev',
+  workerUrl: 'https://sushidza-adminpanel.polihov-alexey-a.workers.dev',
   assetBaseUrl: 'https://xn--80ahjc8bm9a.xn--p1ai',
   passwordStorageKey: 'sushidza_admin_password',
   ordersRefreshMs: 60000
